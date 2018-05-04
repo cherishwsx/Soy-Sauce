81d1205e05b93ddc90e5877ff896d46db825c754
Chinate <szynate@outlook.com>
2018-05-03 23:28:12 -0400

    update decision tree

--- DecisionTree.R
+++ DecisionTree.R
@@ -100,11 +100,12 @@ stopOrNot <- function(myData, depth) {
   return(result)
 }
 
-buildSubTree <- function(myData, classes, depth = 1, featureSpace = list(NA)) {
+buildSubTree <- function(myData, classes, depth = 1, featureSpace = list(NA), random = T) {
   depthsu <- depth
   if (stopOrNot(myData = myData, depthsu)) {
     return(newNode(splitRule = NA, class =paste(as.character(as.data.frame(sort(table(classes), decreasing = T))[1,1])), childT = NA, childF = NA))} else {
-      rulefound <- bestRule(myData = myData[,sample(1:length(myData),round(sqrt(length(myData))))], classes = classes, featureSpace = featureSpace)
+      if (random) {rulefound <- bestRule(myData = myData[,sample(1:length(myData),round(sqrt(length(myData))))], classes = classes, featureSpace = featureSpace)} else {
+      rulefound <- bestRule(myData = myData, classes = classes, featureSpace = featureSpace)}
       if (is.na(rulefound$feature)) {return(newNode(splitRule = NA, class =paste(as.character(as.data.frame(sort(table(classes), decreasing = T))[1,1])), childT = NA, childF = NA))}
       featureSpace <- list.append(featureSpace, rulefound)
       if (is.numeric(myData[1,rulefound$feature])) {
@@ -120,8 +121,8 @@ buildSubTree <- function(myData, classes, depth = 1, featureSpace = list(NA)) {
       }
       if((nrow(subData1)==nrow(myData))||(nrow(subData2)==nrow(myData))) {return(newNode(splitRule = NA, class =paste(as.character(as.data.frame(sort(table(classes), decreasing = T))[1,1])), childT = NA, childF = NA))}
       depth <- depth + 1
-      childT <- buildSubTree(myData = subData1, classes = subClass1, depth = depth, featureSpace = featureSpace)
-      childF <- buildSubTree(myData = subData2, classes = subClass2, depth = depth, featureSpace = featureSpace)
+      childT <- buildSubTree(myData = subData1, classes = subClass1, depth = depth, featureSpace = featureSpace, random = random)
+      childF <- buildSubTree(myData = subData2, classes = subClass2, depth = depth, featureSpace = featureSpace, random = random)
       thisNode <- newNode(splitRule = rulefound, class = NA, childT = childT, childF = childF)
       return(thisNode)
     }
